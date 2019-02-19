c49441abe23c77d13e4c16f2c7edfb3ce80c6744
sergi <sergisiba@gmail.com>
2019-02-19 16:46:31 +0100

    Now with ctype

--- tmp/tcp/server.py
+++ tmp/tcp/server.py
@@ -1,7 +1,15 @@
 import socket
-import struct
+from ctypes import *
 HOST = ''     # Symbolic name meaning all available interfaces
 PORT = 9002   # Arbitrary non-privileged port
+
+
+class Pac1(Structure):
+    _fields_ = [('tipus', c_ubyte ),
+                ('name', c_char*7),
+                ('mac', c_char*13),
+                ('random', c_char*7),
+                ('data', c_char*50)]
 
 
 if __name__ == '__main__':
@@ -15,6 +23,7 @@ if __name__ == '__main__':
         print data
     bties = conn.recv(78)
     print len(bties)
-    print struct.unpack('B7p13s7s50p', bties)
+    struc = Pac1.from_buffer_copy(bties)
+    print struc.type
     conn.close()
     s.close()
